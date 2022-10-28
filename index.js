1a3b538c353faa3e74b74e458efd0f12b42075f0
Janek Schmerling <jschmerling@amexus.com>
2022-10-28 14:47:43 +0200

    jsc

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', (event) => {
     console.log('loaded');
-    
+    console.log(document.querySelector('#website-button'));
     document.querySelector('#website-button').addEventListener('click', function() {
         console.log('website click');
         window.location.replace('https://www.amexus.com/');
